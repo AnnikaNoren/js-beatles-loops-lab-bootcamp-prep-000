9e19c106435c61cd55044e63ea1d753c6826cc3f
Annika Noren <amenoren@gmail.com>
2017-11-04 17:42:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function theBeatlesPlay (musicians, instruments){
+  var array = [];
+
+  for (var i = 0; i <= musicians.length; i++) {
+    //tempString = "\(musicians[i]) plays \(instruments[i])"
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+
+  return array
+}
+
+function johnLennonFacts(facts){
+  var johnLennonFacts = []
+  i = 0
+
+  while (i < facts.length){
+    johnLennonFacts.push(facts[i] + "!!!")
+    i++
+  }
+
+  return johnLennonFacts;
+}
