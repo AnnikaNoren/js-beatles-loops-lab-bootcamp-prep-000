95d3493f88139cc52ee876f0ce61db1a3de8ea6d
Annika Noren <amenoren@gmail.com>
2017-11-04 17:56:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments){
 
   for (var i = 0; i <= musicians.length; i++) {
     //tempString = "\(musicians[i]) plays \(instruments[i])"
-    array.push(musicians[i] + " plays " + instruments[i])
+    array.push(musicians[i] + " plays " + instruments[i]);
   }
 
   return array;
